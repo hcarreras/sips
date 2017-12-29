f02ab5eb22e275c5c4014cd78047d64378eae355
hcarreras <haricarreras@gmail.com>
2017-12-29 13:25:18 +0100

    Add levels
    Parties start slowly. No one dares to do anything at the beginning, but when the party is started, no one wants a soft challenge.
    
    The challenges are defined in the JSON by levels.

--- src/application.js
+++ src/application.js
@@ -1,9 +1,12 @@
 import {setContainerColors, randomIndex, randomColor, hexToComplimentary} from "./utilities"
 import data from '../challenges.json'
 
+const CHALLENGES_PER_LEVEL = 5
+const levels = {1: 'Sober', 2: "Tipsy", 3: "Drunk", 4:"Wasted"}
+let currentLevel = 0
 const challenges = data["challenges"]
-const challengesUncompleted = challenges.slice(0)
-const challengesCompleted = []
+let challengesUncompleted
+let challengesCompleted
 const challengeContainer = document.getElementById("challenge-container")
 
 const getUncompletedChallenge = (challengesUncompleted, challengesCompleted) => {
@@ -22,8 +25,21 @@ const nextChallenge = () => {
 
   challengeContainer.textContent = getUncompletedChallenge(challengesUncompleted, challengesCompleted)
   setContainerColors(challengeContainer, mainColor, secondaryColor)
+  mayLevelUp()
+}
+
+const nextLevel = () => {
+  currentLevel += 1
+  challengesUncompleted = challenges[currentLevel].slice(0)
+  challengesCompleted = []
+}
+
+const mayLevelUp = () => {
+  if(challengesCompleted.length == CHALLENGES_PER_LEVEL)
+    nextLevel()
 }
 
 challengeContainer.addEventListener("click", nextChallenge);
 
+nextLevel()
 nextChallenge()
